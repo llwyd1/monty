6cdb123e5978df6e66d60af7c5dec995c7b94ac6
Frankenstei <40075886+Frankenstei@users.noreply.github.com>
2023-01-19 23:57:56 +0100

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,19 @@
+#include "monty.h"                                                                                 /**
+
+ * stack_func - prints the top
+
+ * @head: head                                                                                      * @num_pos: line_number
+
+ * Return: nothing
+
+*/
+
+void stack_func(stack_t **head, unsigned int num_pos)
+
+{
+
+        (void)head;
+
+        (void)num_pos;
+
+}
