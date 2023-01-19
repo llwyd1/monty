068f54e18d54ea708445fd7de82103e914db44f5
Frankenstei <40075886+Frankenstei@users.noreply.github.com>
2023-01-19 21:25:12 +0100

    nop.c

---
+++ nop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+
+  *nop_func - it does nothing
+
+  *@head:  head
+
+  *@num_pos: stack number
+
+  *Return: nothing
+
+ */
+
+void nop_func(stack_t **head, unsigned int num_pos)
+
+{
+
+        (void) num_pos;
+
+        (void) head;
+
+}
