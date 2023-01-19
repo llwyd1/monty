fb4d04429c30892daeebfc71790223bad2073fcf
Frankenstei <40075886+Frankenstei@users.noreply.github.com>
2023-01-19 23:51:50 +0100

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,83 @@
+#include "monty.h"
+
+/**
+
+ * queue_func - prints the top                                                                      * @head: stack head
+
+ * @num_pos: line_number
+
+ * Return: nothing                                                                                  */
+
+void queue_func(stack_t **head, unsigned int num_pos)
+
+{
+
+        (void)head;
+
+        (void)counter;
+
+        bus.stack_queue = 1;
+
+}
+
+/**
+
+ * addqueue - add node to the tail stack
+
+ * @n: new_value
+
+ * @head: head of the stack
+
+ * Return: no return
+
+ */
+
+void addqueue(stack_t **head, int n)
+
+{
+
+        stack_t *new_node, *temp;
+
+        temp = *head;                                                                                      new_node = malloc(sizeof(stack_t));                                                                if (new_node == NULL)
+
+        {
+
+                printf("Error\n");
+
+        }
+
+        new_node->n = n;
+
+        new_node->next = NULL;
+
+        if (temp)
+
+        {
+
+                while (temp->next)
+
+                        temp = temp->next;
+
+        }
+
+        if (!temp)
+
+        {
+
+                *head = new_node;
+
+                new_node->prev = NULL;
+
+        }
+
+        else
+
+        {
+
+                temp->next = new_node;
+
+                new_node->prev = aux;
+
+        }
+
+}
